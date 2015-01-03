439544bbc53d98ffeb7c018772a9ce2a7a52df00
gau820827 <b00902088@ntu.edu.tw>
2015-01-04 06:27:00 +0800

    Add the tool to parse picture from Google

---
+++ tools/googleparser.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import urllib2
+import re
+import sys
+import json
+from subprocess import call
+
+print "Chinese target is not available now."
+print "Enter the target you want to search:"
+name = raw_input()
+print "Enter the number you want to fetch:"
+data = raw_input()
+number_of_picture = -1
+for time in xrange(0, int(data)):
+	start = time*8
+	url = ('https://ajax.googleapis.com/ajax/services/search/images?' +
+	       'v=1.0&q=' + name + '&userip=INSERT-USER-IP&rsz=8&start=' + str(start))
+	request = urllib2.Request(url)
+	response = urllib2.urlopen(request)
+	# Process the JSON string.
+	results = json.load(response)
+	#print results
+	for count in xrange(0,8):
+		number_of_picture = number_of_picture + 1
+		target = results['responseData']['results'][count]['url']
+		if ".jpg" in target:
+			filename = str(number_of_picture) + "p.jpg"
+		else:
+			filename = str(number_of_picture) + "p.png"
+		try:
+			req = urllib2.urlopen(target).read()
+			picture = open(filename, "wb")
+			picture.write(req)
+			picture.close()
+			print "Download " + target
+		except:
+			number_of_picture = number_of_picture-1
+			print "Download " + target + " fail!"
